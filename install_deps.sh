89270d4d83bc932c11b01fc40d9a691ffbaf82f4
stwind <stwindfy@gmail.com>
2018-01-15 08:53:29 +0800

    update config

--- install_deps.sh
+++ install_deps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DEPS=(z nvm zsh-syntax-highlighting tmux yarn reattach-to-user-namespace terminal-notifier fd httpie ripgrep)
+DEPS=(z nvm zsh-syntax-highlighting tmux yarn reattach-to-user-namespace terminal-notifier fd httpie ripgrep pwgen)
 
 brew install "${DEPS[@]}"
