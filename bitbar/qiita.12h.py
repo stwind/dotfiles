c85375c8f8aac40477af88248e5e1258502c8f43
stwind <stwindfy@gmail.com>
2019-07-21 17:42:25 +0800

    Update qiita

--- bitbar/qiita.12h.py
+++ bitbar/qiita.12h.py
@@ -11,9 +11,11 @@
 # encoding=utf8
 
 import sys
+import codecs
 import json
 import urllib.parse
 import urllib.request
+from urllib.request import ProxyHandler, build_opener, Request
 from bs4 import BeautifulSoup
 
 
@@ -21,48 +23,10 @@ tags = sorted(['python', 'javascript', 'typescript',
                'scala', 'GLSL', 'ディープラーニング', 'tensorflow', 'pytorch', '機械学習', '深層学習','3D','opencv',
                'machinelearning', 'deeplearning', 'webgl', 'cg', 'erlang', 'rust'])
 
+tags = ["python", "glsl"]
 
-def request(url, data):
-    dataBytes = json.dumps(data).encode('utf-8')
-    request = urllib.request.Request(url, headers={
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36",
-        "Pragma": "no-cache",
-        "Content-Type": "application/json;charset=UTF-8",
-        "Accept": "application/json",
-        "Origin": "https://qiita.com",
-        "referer": "https://qiita.com/tags/python"
-    }, data=dataBytes)
-    return urllib.request.urlopen(request)
-
-
-def make_soup(url):
-    try:
-        return BeautifulSoup(request(url).read(), 'html.parser')
-    except Exception as e:
-        return None
-
-
-def parse_item(item):
-    title = item.get_text()
-    href = item['href']
-    return title, 'https://qiita.com{}'.format(href)
-
-
-def print_tag(tag, cate):
-    for i in range(1, 4):
-        soup = make_soup('https://qiita.com/tags/{}/{}?page={}'.format(urllib.parse.quote(tag), cate, i))
-        for item in soup.select('.tsf-ArticleBody_title'):
-            title, href = parse_item(item)
-            print('--{} | size=12 href={}'.format(title, href))
-
-
-def run_query(query):
-    data = {'query': query, "variables":{"urlName":"python","page":1,"per":10}}
-    return request("https://qiita.com/graphql", data)
-
-
-if __name__ == '__main__':
-    query = """query getTagArticles($urlName: String!, $page: Int!, $per: Int!) {
+QUERY = """
+query getTagArticles($urlName: String!, $page: Int!, $per: Int!) {
   tag(urlName: $urlName) {
     ...TagArticles
   }
@@ -73,9 +37,6 @@ fragment TagArticles on Tag {
     items {
       ...TagArticlesItems
     }
-    pageData {
-      ...TagArticlesPageData
-    }
   }
 }
 
@@ -83,32 +44,59 @@ fragment TagArticlesItems on Article {
   createdAt
   uuid
   title
-  likesCount
   author {
-    profileImageUrl
-    urlName
-  }
-  tags {
-    name
     urlName
   }
 }
+"""
 
-fragment TagArticlesPageData on PageData {
-  currentPage
-  isFirstPage
-  isLastPage
-  nextPage
-  prevPage
-  totalPages
-} """
-    print(run_query("query getTagArticles($urlName: String!, $page: Int!, $per: Int!) {\n  tag(urlName: $urlName) {\n    ...TagArticles\n  }\n}\n\nfragment TagArticles on Tag {\n  articles(page: $page, per: $per) {\n    items {\n      ...TagArticlesItems\n    }\n    pageData {\n      ...TagArticlesPageData\n    }\n  }\n}\n\nfragment TagArticlesItems on Article {\n  createdAt\n  uuid\n  title\n  likesCount\n  author {\n    profileImageUrl\n    urlName\n  }\n  tags {\n    name\n    urlName\n  }\n}\n\nfragment TagArticlesPageData on PageData {\n  currentPage\n  isFirstPage\n  isLastPage\n  nextPage\n  prevPage\n  totalPages\n}\n"))
 
-    # print("qiita")
-    # print("---")
-    # print("Refresh... | refresh=true")
-    # print("---")
-    # for tag in tags:
-    #     print(tag)
-    #     print_tag(tag, 'items')
-        # print_tag(tag, 'likes')
+def get_opener():
+    proxy = '127.0.0.1:1235'
+    proxy_handler = ProxyHandler({
+        'http': 'http://' + proxy,
+        'https': 'https://' + proxy
+    })
+    return build_opener(proxy_handler)
+
+
+opener = get_opener()
+
+
+def request(url, tag,  data):
+    dataBytes = json.dumps(data).encode('utf-8')
+    request = Request(url, headers={
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36",
+        "Pragma": "no-cache",
+        "Content-Type": "application/json;charset=UTF-8",
+        "Accept": "application/json",
+        "Origin": "https://qiita.com",
+        "referer": "https://qiita.com/tags/{}".format(urllib.parse.quote(tag).lower()),
+        "Cookie": "_qiita_login_session=UlVFZnRBNy9LK2Q3YUo4SmxpTzdlbURFRjVEVlk2VzNSTE1GbDFlSngyTEplVHMydmgvZnIxWWNzYllKeTRqVmdLMlV0S3RVTkJqN0dXWUZhRHJnRkNCQ2pmYm5YNnFtNG8wbXdsU1c0ZzdCUVRFRTV0VDlQR3EwTndkMTdzRFBObHI3YXhzRlAvYXdWdlBkWWVhd3Vpb1Y2YUpycUdQUkF1Vml6NzVXZG1TYWd1NXpkNDdQUmR4aXpXS0JONEtQLS1zc0JLSG80NUtBc2JwMm8xdnZmK3ZBPT0%3D--1308176661ea144bbe650811108e22b074e3e9fa",
+        "X-CSRF-Token": "PAImJayuPs5vbVXblad4yRwpe5Ut2LawlzGYx5qjebrRBzA8JGu5FrdJ60c6qxwDg4PXUcc4WSZAKc44y65p5g=="
+    }, data=dataBytes)
+    return opener.open(request)
+
+
+def run_query(tag):
+    data = {'query': QUERY, "variables":{"urlName":urllib.parse.quote(tag).lower(),"page":1,"per":80}}
+    resp = request("https://qiita.com/graphql", tag, data)
+    reader = codecs.getreader("utf-8")
+    return json.load(reader(resp))
+
+
+def print_tag(tag):
+    data = run_query(tag)
+    for item in data['data']['tag']['articles']['items']:
+        href = "https://qiita.com/{}/items/{}".format(item["author"]["urlName"],item["uuid"])
+        print('--{} | size=12 href={}'.format(item["title"], href))
+
+
+if __name__ == '__main__':
+    print("qiita")
+    print("---")
+    print("Refresh... | refresh=true")
+    print("---")
+    for tag in tags:
+        print(tag)
+        print_tag(tag)
